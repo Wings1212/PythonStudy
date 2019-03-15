2f1576163d1fc9fc3ff456c51e75684ef8f0185e
Wings1212 <44222648+Wings1212@users.noreply.github.com>
2019-03-15 19:19:21 +0800

    '抽象基类的简单使用'

---
+++ abstractclass.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractclassmethod
+import webbrowser
+
+
+class MyClass(ABC):
+    @abstractclassmethod
+    def study(cls, *args, **kwargs):
+        pass
+
+    @abstractclassmethod
+    def read(cls, *args, **kwargs):
+        pass
+
+
+class Student(MyClass):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def study(self, *args, **kwargs):
+        dream = input('{0},你的梦想是什么？'.format(self.name))
+        if dream == 'money':
+            print('you are not good')
+        else:
+            print('ok, you are good')
+
+    def read(self, *args, **kwargs):
+        try:
+            webbrowser.open('http://www.baidu.com')
+        except Exception as ret:
+            print(ret)
+            raise ValueError from None
+        else:
+            print('the website was opened')
+
+
+if __name__ == '__main__':
+    student = Student('胡子鹏', 21)
+    # 不能实例化抽象基类，继承于抽象基类的类必须重写其抽象方法要不也没法实例化
+    h = MyClass()
+    student.study()
+    print("*" * 50)
+    student.read()
